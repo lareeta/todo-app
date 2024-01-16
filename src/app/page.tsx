1397af2259ddee1f251bf290f02cc493ef761c65
ritaRakhimova <51830833+lapari@users.noreply.github.com>
2024-01-16 23:11:46 +0100

    refactor: extract svg to separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+function CloseIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+    >
+      <path
+        d="M18 1.81286L16.1871 0L9 7.18714L1.81286 0L0 1.81286L7.18714 9L0 16.1871L1.81286 18L9 10.8129L16.1871 18L18 16.1871L10.8129 9L18 1.81286Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
 export default function Home() {
   return (
     <div className="container">
@@ -12,18 +29,7 @@ export default function Home() {
             <span>Make UI design in Figma</span>
           </label>
           <button className="buttonItem">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-            >
-              <path
-                d="M18 1.81286L16.1871 0L9 7.18714L1.81286 0L0 1.81286L7.18714 9L0 16.1871L1.81286 18L9 10.8129L16.1871 18L18 16.1871L10.8129 9L18 1.81286Z"
-                fill="currentColor"
-              />
-            </svg>
+            <CloseIcon />
           </button>
         </div>
         <div className="taskItem">
@@ -32,18 +38,7 @@ export default function Home() {
             <span>Implement the markup with pure HTML/CSS</span>
           </label>
           <button className="buttonItem">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-            >
-              <path
-                d="M18 1.81286L16.1871 0L9 7.18714L1.81286 0L0 1.81286L7.18714 9L0 16.1871L1.81286 18L9 10.8129L16.1871 18L18 16.1871L10.8129 9L18 1.81286Z"
-                fill="currentColor"
-              />
-            </svg>
+            <CloseIcon />
           </button>
         </div>
         <div className="taskItem">
@@ -52,18 +47,7 @@ export default function Home() {
             <span>Make the markup interactive using pure JavaScript</span>
           </label>
           <button className="buttonItem">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-            >
-              <path
-                d="M18 1.81286L16.1871 0L9 7.18714L1.81286 0L0 1.81286L7.18714 9L0 16.1871L1.81286 18L9 10.8129L16.1871 18L18 16.1871L10.8129 9L18 1.81286Z"
-                fill="currentColor"
-              />
-            </svg>
+            <CloseIcon />
           </button>
         </div>
         <div className="taskItem">
@@ -72,18 +56,7 @@ export default function Home() {
             <span>Publish the repository in GitHub</span>
           </label>
           <button className="buttonItem">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-            >
-              <path
-                d="M18 1.81286L16.1871 0L9 7.18714L1.81286 0L0 1.81286L7.18714 9L0 16.1871L1.81286 18L9 10.8129L16.1871 18L18 16.1871L10.8129 9L18 1.81286Z"
-                fill="currentColor"
-              />
-            </svg>
+            <CloseIcon />
           </button>
         </div>
         <div className="taskItem">
@@ -92,18 +65,7 @@ export default function Home() {
             <span>Deploy the app to Netlify</span>
           </label>
           <button className="buttonItem">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-            >
-              <path
-                d="M18 1.81286L16.1871 0L9 7.18714L1.81286 0L0 1.81286L7.18714 9L0 16.1871L1.81286 18L9 10.8129L16.1871 18L18 16.1871L10.8129 9L18 1.81286Z"
-                fill="currentColor"
-              />
-            </svg>
+            <CloseIcon />
           </button>
         </div>
         <div className="taskItem">
@@ -112,18 +74,7 @@ export default function Home() {
             <span>Read and write tasks from/to local storage</span>
           </label>
           <button className="buttonItem">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-            >
-              <path
-                d="M18 1.81286L16.1871 0L9 7.18714L1.81286 0L0 1.81286L7.18714 9L0 16.1871L1.81286 18L9 10.8129L16.1871 18L18 16.1871L10.8129 9L18 1.81286Z"
-                fill="currentColor"
-              />
-            </svg>
+            <CloseIcon />
           </button>
         </div>
         <div className="taskItem">
@@ -132,18 +83,7 @@ export default function Home() {
             <span>Rewrite the app using React.js</span>
           </label>
           <button className="buttonItem">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-            >
-              <path
-                d="M18 1.81286L16.1871 0L9 7.18714L1.81286 0L0 1.81286L7.18714 9L0 16.1871L1.81286 18L9 10.8129L16.1871 18L18 16.1871L10.8129 9L18 1.81286Z"
-                fill="currentColor"
-              />
-            </svg>
+            <CloseIcon />
           </button>
         </div>
       </div>
